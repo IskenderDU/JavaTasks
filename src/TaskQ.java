c339cfff137c8283782b21f7708146b02d34ccf9
Iskender Duulatbekov <duulatbekoviskender1@gmail.com>
2026-09-24 13:41:25 +0600

    TaskQ

---
+++ src/TaskQ.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TaskQ {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        System.out.println(m / n);
+    }
+}
