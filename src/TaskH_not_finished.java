f83218caa08e2796c6cc13155f51825fc0c1b73f
IskenderDU <duulatbekoviskender1@gmail.com>
2026-09-24 11:16:24 +0600

    TaskH_is_finished

--- src/TaskH_not_finished.java
+++ src/TaskH_not_finished.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
-public class TaskH_not_finished {
+public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner (System.in);
         int n = in.nextInt();
-        int b = (n % 100);
-        int a = (n / 10)-(n /100);
-        System.out.println(a);
+        int c = n % 10;
+        int b = (n / 10) % 10;
+        int a = (n / 100) % 10;
+        System.out.println(b);
     }
 }
