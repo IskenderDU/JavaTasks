e2332c5af3d64f673d43f46b873a0bc8f5b0c959
Iskender Duulatbekov <duulatbekoviskender1@gmail.com>
2026-09-24 13:34:06 +0600

    TaskJ

---
+++ src/TaskJ.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class TaskJ {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println((n + 2) - (n % 2));
+    }
+}
