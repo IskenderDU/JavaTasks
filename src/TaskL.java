42a5def357d44888e36c6f5a4833fc3920c03e5c
IskenderDU <duulatbekoviskender1@gmail.com>
2026-09-26 02:40:06 +0600

    TaskL

--- src/TaskL.java
+++ src/TaskL.java
@@ -1,13 +1,28 @@
 import java.util.Scanner;
 
 public class TaskL {
+    private static Object String;
+
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int Totalhr = (n / 3600) % 24;
-        int Totalmin = (n / 60);
+        int Totalmin = (n / 60) % 60;
         int Ttlsec = (n % 60);
-        System.out.println(Totalhr + ":" + Totalmin + ":" + Ttlsec);
+
+        char zero = '0';
+
+        String Ttlmin = "" + Totalmin;
+        String Ttlsc = "" + Ttlsec;
+        if (Totalmin < 10) {
+            Ttlmin = "" + zero + Totalmin;
+        }
+
+        if (Ttlsec < 10) {
+            Ttlsc = "" + zero + Ttlsec;
+        }
+
+        System.out.println(Totalhr + ":" + Ttlmin + ":" + Ttlsc);
     }
 }
